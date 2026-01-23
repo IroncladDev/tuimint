4df11f79d781a913203d95ecd8783ad327c4d194
IroncladDev <conner@ironclad.sh>
2026-01-23 13:59:21 -0600

    splash screen

--- src/ui/screens/splash.rs
+++ src/ui/screens/splash.rs
@@ -2,7 +2,12 @@ use std::sync::{Arc, Mutex};
 
 use color_eyre::Result;
 use crossterm::event::{KeyCode, KeyEvent};
-use ratatui::{style::Style, widgets::Block};
+use ratatui::{
+    layout::{Alignment, Constraint},
+    style::{Color, Style},
+    text::{Line, Span, Text},
+    widgets::{Block, Padding, Paragraph},
+};
 use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{
@@ -17,10 +22,57 @@ impl UIScreen for SplashScreen {
     fn render(frame: &mut ratatui::Frame, state: &Arc<Mutex<AppState>>) {
         let mut state = state.lock().unwrap();
         state.increment();
-        let text = format!("Count: {} {:?}", state.count, state.screen);
-        let paragraph = ratatui::widgets::Paragraph::new(text);
-        let block = Block::bordered().border_style(Style::new().blue().on_white().bold().italic());
-        frame.render_widget(paragraph.clone().block(block), frame.area());
+
+        let lines = vec![
+            Line::from("           ⠰⣉⠆           ").style(Color::Rgb(86, 102, 130)),
+            Line::from("      ⢎⡱  ⣀⠤⠤⠤⣀  ⢎⡱      ").style(Color::Rgb(86, 102, 130)),
+            Line::from(vec![
+                Span::from("▀█▀ █ █ █"),
+                Span::from("⠊").style(Color::Rgb(86, 102, 130)),
+                Span::from("▟▀█▀▙"),
+                Span::from("⠑").style(Color::Rgb(86, 102, 130)),
+                Span::from("█ █▀█ ▀█▀"),
+            ]),
+            Line::from(vec![
+                Span::from(" █  █▄█ █"),
+                Span::from("⡀").style(Color::Rgb(86, 102, 130)),
+                Span::from("▜ ▀ ▛"),
+                Span::from("⢀").style(Color::Rgb(86, 102, 130)),
+                Span::from("█ █ █  █ "),
+            ]),
+            Line::from("     ⠰⣉⠆ ⠈⠒⠤⠤⠤⠒⠁ ⠰⣉⠆     ").style(Color::Rgb(86, 102, 130)),
+            Line::from("         ⡔⢢   ⡔⢢         ").style(Color::Rgb(86, 102, 130)),
+            Line::from("         ⠈⠁   ⠈⠁         ").style(Color::Rgb(86, 102, 130)),
+            Line::from(""),
+            Line::from(vec![
+                Span::from("- ["),
+                Span::from("w").style(Style::default().fg(Color::Yellow)),
+                Span::from("] Wallets    "),
+            ]),
+            Line::from(vec![
+                Span::from("- ["),
+                Span::from("j").style(Style::default().fg(Color::Yellow)),
+                Span::from("] Join a Mint"),
+            ]),
+            Line::from(vec![
+                Span::from("- ["),
+                Span::from("w").style(Style::default().fg(Color::Yellow)),
+                Span::from("] Settings   "),
+            ]),
+        ];
+
+        let text = Text::from(lines).style(Style::default().fg(Color::Blue));
+        let paragraph = Paragraph::new(text).alignment(Alignment::Center);
+
+        let block = Block::bordered()
+            .border_style(Style::new().blue())
+            .padding(Padding::vertical(1));
+        frame.render_widget(
+            paragraph.clone().block(block),
+            frame
+                .area()
+                .centered(Constraint::Max(60), Constraint::Max(16)),
+        );
     }
 
     fn handle_keys(
